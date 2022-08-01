a6c7b336a465ce5572f068bf2c240346879d6c36
Lazuardi1 <87347424+Lazuardi1@users.noreply.github.com>
2022-08-01 04:31:11 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/index.html"],
+  theme: {
+    extend: {
+      colors:{
+        "ungu" : "#5267DF",
+        "merah" : "#FA5959",
+        "biru" : "#2A2A45",
+        "abu-abu" : "#91949A2",
+        "putih" : "#f7f7f7"
+      },
+
+      fontFamily:{
+        Poppins : ["Poppins, sans-serif"]
+      },
+      container:{
+        center:true,
+        padding:"1rem",
+        screens:{
+          lg:"1124px",
+          xl:"1124px",
+          "2xl": "1124px",
+        },
+
+        
+      }
+      
+    },
+  },
+  plugins: [],
+}
